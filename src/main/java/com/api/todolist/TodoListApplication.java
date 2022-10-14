a7674ca06732bf028d128a075757c8026b3b211d
WeltonLimaBonfim <56077585+WeltonLimaBonfim@users.noreply.github.com>
2022-10-14 18:01:44 -0300

    Update TodoListApplication.java

--- src/main/java/com/api/todolist/TodoListApplication.java
+++ src/main/java/com/api/todolist/TodoListApplication.java
@@ -2,7 +2,7 @@ package com.api.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
